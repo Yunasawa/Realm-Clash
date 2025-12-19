0b2b0f88542675073ca060d7034f343055771d58
4xioM-ai <nguyenkhacquang.work@gmail.com>
2025-12-19 13:11:46 +0700

    pop stash

--- Clients/ClientDefinition.hpp
+++ Clients/ClientDefinition.hpp
@@ -20,7 +20,7 @@
 #define CODE_NULL								"NULL"
 
 // Client phases
-<<<<<<< Updated upstream
+
 #define PHASE_WELCOME							000
 
 #define PHASE_LOBBY_JOINING_READY				100
@@ -30,14 +30,6 @@
 #define PHASE_LOBBY_JOINED_RLEADER				111                
 #define PHASE_LOBBY_JOINED_TLEADER				112                
 #define PHASE_LOBBY_JOINED_RTLEADER				113     
-=======
-#define PHASE_WELCOME                       000 /* thông điệp welcome */
-#define PHASE_LOBBY_JOINING                 100 /* state */
-#define PHASE_LOBBY_JOINED_MEMBER           101 /* role thành viên */
-#define PHASE_LOBBY_JOINED_RLEADER          102                
-#define PHASE_LOBBY_JOINED_TLEADER          103                
-#define PHASE_LOBBY_JOINED_RTLEADER         104                
->>>>>>> Stashed changes
 
 #define PHASE_GAME_MAP_IDLING					200
 
